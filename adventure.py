bb95de4a2be9390f6cac499d9e9ec02fbd1f4fff
Art4v <aarav_20@outlook.com>
2026-01-18 19:36:55 +1100

    improved starting animation

--- adventure.py
+++ adventure.py
@@ -1,21 +1,55 @@
 # libraries
+from random import randint
 from time import sleep
 
 
-# intro card
-print("*** WELCOME TO YOUR FOREST ADVENTURE ***")
-sleep(1)
-print("This is a text-based adventure game where your mission is to find the lost treasure of the forest. Select options by inputting the digits 1, 2, 3, or 4 when prompted!")
-sleep(1)
-if input("Would you like to begin your journey? (y/n) ").lower() == "y":
-    sleep(1)
-    print("Good luck!")
-    print("\n***\n")
-    sleep(1)
-else:
-    print("Come Back Soon!")
-    exit()
+# loading animation
+def load(n, speed):
+    for _ in range(randint(3, 8)):
+            sleep(speed / 5)
+            print(".", end="")
+            sleep(speed / 5)
+            print(".", end="")
+            sleep(speed / 5)
+            print(".")
+    sleep(speed)
+    print("\n!!!\n")
+    sleep(speed / 2)
 
-# main game
-print("Journey Under Construction...")
+
+# greeting function
+def start(speed):
+    print("*** WELCOME TO YOUR FOREST ADVENTURE ***")
+    sleep(speed)
+    print("This is a text-based AI-powered adventure game where your mission is to find the lost treasure of the forest. Select options by inputting the digits 1, 2, 3, or 4 when prompted. To exit at any time, type 'exit'")
+    sleep(speed)
+    while True:
+        # get user input
+        start = input("Would you like to begin your journey? (y/n) ")
+        # start or exit game based on input
+        if start.lower() == "y":
+            sleep(speed)
+            print("Loading Journey...")
+            load(3, speed)
+            print("Good luck!")
+            print("\n***\n")
+            sleep(speed)
+            break
+        elif start.lower() in ["n", "exit"]:
+            print("Come Back Soon!")
+            exit()
+        
+    game()
+    return  
+     
+# game start function
+def game():
+    print("Journey Under Construction")
+    return
+
+
+# call function
+start(1)
+
+
 
